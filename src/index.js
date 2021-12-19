b3f4c2af8f292d24cf667d61715e43274d1b5bf4
Muhammad <maxkarimov077@gmail.com>
2021-12-20 01:15:12 +0300

    Препроцессор JSX практика

--- src/index.js
+++ src/index.js
@@ -1,49 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-
-
-const elem = (
-    <div>
-
-    </div>
-)
-
-const Header = () => {
-    return <h1>Hello World!</h1>
-}
-const Field =() => {
-    const holder = "Enter here"
-    const styledField = {
-        width: '300px'
-    }
-    return <input 
-        type="text" 
-        style={styledField}
-        placeholder={holder}/>
-}
-const Btn = () => {
-    const text = "Log in"
-    const logged = false
-    return <button>{logged ? null : text}</button>
-}
-
-const App = () => {
-    return(
-        <div>
-            <Header/>
-            <Field/>
-            <Btn/>
-        </div>
-    )
-}
+import App from './components/app/';
 
 
 ReactDOM.render(
-  <App/>
-/*   <React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode> */,
+  </React.StrictMode>,
   document.getElementById('root')
 );
