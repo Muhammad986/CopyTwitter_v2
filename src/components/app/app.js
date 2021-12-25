8c4f8c7b5fab6ed415116ad1bb5630bf5b9be426
Muhammad <maxkarimov077@gmail.com>
2021-12-25 05:48:17 +0300

    Создаем собственные события и работаем с иммутабильностью 94

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -13,25 +13,59 @@ import '../post-list/post-list.css';
 import '../post-list-item/post-list-item.css';
 import '../search-panel/search-panel.css';
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'That is so good', important: false, id: 2},
+                {label: 'I need a break...', important: false, id: 3}
+            ]
+        }
+        this.maxId = 4;
+    }
 
-    const data = [
-        {label: 'Going to learn React', important: true, id: 'dhtd'},
-        {label: 'That is so good', important: false, id: 'wtwdt'},
-        {label: 'I need a break...', important: false, id: 'dfdfd'}
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>                
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem = (body) => {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>                
+                </div>
+                <PostList 
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <PostAddForm
+                    onAdd={this.addItem}/>
             </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    )
+        )
+    }
 }
 
-export default App;
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className="bottom-panel d-flex">
+        <div className="bottom-panel d-flex">
             <input
                 type="text"
                 placeholder="О чём вы думаете сейчас?"
                 className="form-control new-post-label"
             />
             <button
-            type="submit"
-            className="btn btn-outline-secondary"
+                type="submit"
+                className="btn btn-outline-secondary"
+                onClick={() => onAdd('Hello')}
             >Добавить</button>
-        </form>
+        </div>
     )
 }
 
